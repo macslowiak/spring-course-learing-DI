14046f3ae1a3e3ab37ff9f955baf3be75d9b1181
Maciej Słowiak <slowiakmaciej@gmail.com>
2022-05-10 12:26:59 +0200

    Adjust property/setter/constructor to Spring

--- src/main/java/guru/springframework/sfgdi/controllers/SetterInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/SetterInjectedController.java
@@ -1,14 +1,20 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
 
 /**
  * Created by jt on 12/26/19.
  */
+
+@Controller
 public class SetterInjectedController {
 
     private GreetingService greetingService;
 
+    @Autowired
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
