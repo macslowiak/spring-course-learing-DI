bdb1edb031b3a96ad61883061fb0c812b565cb87
Maciej Słowiak <slowiakmaciej@gmail.com>
2022-05-10 12:35:42 +0200

    Add using @Qualifier lesson

--- src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 /**
@@ -10,11 +11,11 @@ import org.springframework.stereotype.Controller;
 public class ConstructorInjectedController {
     private final GreetingService greetingService;
 
-    public ConstructorInjectedController(GreetingService greetingService) {
+    public ConstructorInjectedController(@Qualifier("constructorGreetingService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
-    public String getGreeting(){
+    public String getGreeting() {
         return greetingService.sayGreeting();
     }
 }
