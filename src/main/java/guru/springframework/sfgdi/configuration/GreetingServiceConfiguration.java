1e083cce87234f7148b5bb4de50996f4ad276931
Maciej Słowiak <slowiakmaciej@gmail.com>
2022-05-11 09:56:04 +0200

    Add Java Configuration Example

---
+++ src/main/java/guru/springframework/sfgdi/configuration/GreetingServiceConfiguration.java
@@ -0,0 +1,26 @@
+package guru.springframework.sfgdi.configuration;
+
+import guru.springframework.sfgdi.services.ConstructorGreetingService;
+import guru.springframework.sfgdi.services.PropertyInjectedGreetingService;
+import guru.springframework.sfgdi.services.SetterInjectedGreetingService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GreetingServiceConfiguration {
+
+    @Bean
+    ConstructorGreetingService constructorGreetingService(){
+        return new ConstructorGreetingService();
+    }
+
+    @Bean
+    PropertyInjectedGreetingService propertyInjectedGreetingService(){
+        return new PropertyInjectedGreetingService();
+    }
+
+    @Bean
+    SetterInjectedGreetingService setterInjectedGreetingService(){
+        return new SetterInjectedGreetingService();
+    }
+}
