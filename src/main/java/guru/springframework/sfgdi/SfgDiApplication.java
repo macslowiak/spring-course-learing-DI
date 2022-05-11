115e4d0580177fe31c8ef63b10115bcc9a5a5d3b
Maciej Słowiak <slowiakmaciej@gmail.com>
2022-05-11 13:54:42 +0200

    Add application.properties with environments example

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -55,7 +55,7 @@ public class SfgDiApplication {
 		PrototypeBean prototypeBean2 = ctx.getBean(PrototypeBean.class);
 		System.out.println(prototypeBean2.getMyScope());
 
-		System.out.println("---- Properties From datasource.properties  ---------");
+		System.out.println("---- Properties From application.properties  ---------");
 		FakeDataSource fakeDataSource = ctx.getBean(FakeDataSource.class);
 		System.out.println(fakeDataSource.getUsername());
 		System.out.println(fakeDataSource.getPassword());
